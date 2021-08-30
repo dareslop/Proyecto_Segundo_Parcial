b2979099b795702c659a11320941e6a1e8bb6bc5
dvega23 <87511013+dvega23@users.noreply.github.com>
2021-08-29 20:06:31 -0500

    Update PrimaryController.java

--- MyCity/src/main/java/grupo6/mycity/PrimaryController.java
+++ MyCity/src/main/java/grupo6/mycity/PrimaryController.java
@@ -1,28 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package grupo6.mycity;
 
 import Modelos.Ciudad;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 
 /**
  * FXML Controller class
  *
  * @author Emanuel Guerrero
+ * @author Diego Vega
+ * @author Darien L
  */
-public class PrimaryController implements Initializable {
+public class GameController implements Initializable {
  
     @FXML
     private GridPane grdPane;
@@ -32,34 +32,48 @@ public class PrimaryController implements Initializable {
     @FXML
     private Label lbDias;
     @FXML
-    private ColumnConstraints grPane;
+    private Label lbMoney;
+    @FXML
+    private Label lbNombreCity;
+    @FXML
+    private Button btVecindario;
+    @FXML
+    private Button btServicio;
+    @FXML
+    private Button btDemolicion;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        city = new Ciudad("facil","Imagenes//grassTile.png");
-        for(int i=0;i< city.getColumns();i++){
-            for(int j=0;j<city.getRows();j++){
-                //InputStream input = App.class.getResourceAsStream(city.getFloor());
-                ImageView img = new ImageView(new Image(city.getFloor()));
-                grdPane.add(img, j, i);                
-            }
-        }      
+             
         
+    }
         
-    }  
-    public class Dias implements Runnable {
-        @Override
-        public void run() {
-            
-            try {
-                city.increaseDays();
-                Thread.sleep(3000);
-                lbDias.setText(String.valueOf(city.getDias()));
-            } catch (InterruptedException ex) {
-                ex.printStackTrace();
+    public void begin(){
+        for(int i = 0;i<30;i++){
+            for(int j=0;j<18;j++){
+                ImageView img = new ImageView(new Image("Imagenes//grassTile.png",50,50,false,false));                
+                grdPane.add(img, i, j);
+            }       
+        Thread t1 = new Thread(()->aumentarDias());
+        t1.start();
+        }
+    }
+    
+    public void aumentarDias(){
+          while(true){
+              try {
+                  city.increaseDays();
+                  Platform.runLater(()->lbDias.setText("dias: "+city.getDias()));                  
+                  Thread.sleep(3000);
+              } catch (InterruptedException ex) {
+                  ex.printStackTrace();
+                }
             }
-            
+        }
+    public void cobrarImpuestos(){            
+        }
+    public void crearServicio(){
+        
         }
     }
         
-}
