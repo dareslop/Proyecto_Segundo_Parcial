97b52e9d18a2610e0ab94c473a955d6ca378a0a8
lopez70-lab <83101819+lopez70-lab@users.noreply.github.com>
2021-08-30 23:09:50 -0500

    Update Residencial.java
    actualizacion

--- MyCity/src/main/java/Modelos/Construcciones/Residencial.java
+++ MyCity/src/main/java/Modelos/Construcciones/Residencial.java
@@ -3,6 +3,8 @@ package Modelos.Construcciones;
 
 
 public class Residencial extends Construcciones{
+    private int cantServicios;
+    
 
     public Residencial(double alumbradoPublico, double valorAgua, double impuestoSueloMesual, int row, int column) {
         super(alumbradoPublico, valorAgua, impuestoSueloMesual, row, column);
