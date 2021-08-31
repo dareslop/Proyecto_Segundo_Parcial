d8524c9ba243a2c29c3b0a49f96a68285b0dd2e8
emasguer <78041019+emasguer@users.noreply.github.com>
2021-08-30 23:24:21 -0500

    Update Servicios.java

--- MyCity/src/main/java/Modelos/Servicios/Servicios.java
+++ MyCity/src/main/java/Modelos/Servicios/Servicios.java
@@ -1,15 +1,20 @@
 
 package Modelos.Servicios;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+
 
 /**
  *
  * @author Emanuel Guerrero
  */
 public class Servicios {
-    protected ArrayList<File> files = new ArrayList<>();        
+    protected ArrayList<String> files = new ArrayList<>();        
     protected int precioConstruccion;
     protected  int costoMensual;
     
@@ -18,17 +23,32 @@ public class Servicios {
         this.precioConstruccion = precioConstruccion;
         this.costoMensual = costoMensual;
     }
-    public void listf(String directoryName, ArrayList<File> files) {
-    File directory = new File(directoryName);    
-    File[] fList = directory.listFiles();
-    if(fList != null)
-        for (File file : fList) {      
-            if (file.isFile()) {
-                files.add(file);
-            } else if (file.isDirectory()) {
-                listf(file.getAbsolutePath(), files);
+    public void listf(String directoryName){
+        try{
+            InputStreamReader lector = new InputStreamReader(Servicios.class.getResourceAsStream(this.toString()+"/"+directoryName+".txt"));
+            BufferedReader reader = new BufferedReader(lector);      
+            String line = reader.readLine();            
+            while((line = reader.readLine()) != null){                
+                files.add(line);                
             }
+         } 
+         catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
-}
+               
+    }
+
+    public ArrayList<String> getFiles() {
+        return files;
+    }
+    
+
+    @Override
+    public String toString() {
+        String sub = this.getClass().getName();
+        return sub.substring(18, sub.length());
+    }    
     
 }
