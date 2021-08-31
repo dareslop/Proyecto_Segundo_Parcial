cc34968da6725e7ad53784b833724b7060483dbb
lopez70-lab <83101819+lopez70-lab@users.noreply.github.com>
2021-08-30 23:05:28 -0500

    Update Vecindario.java
    actualizacion

--- MyCity/src/main/java/Modelos/Vecindario.java
+++ MyCity/src/main/java/Modelos/Vecindario.java
@@ -1,20 +1,63 @@
-
 package Modelos;
 
 import Modelos.Construcciones.Construcciones;
 import Modelos.Servicios.Servicios;
 import java.util.ArrayList;
+import javafx.util.Pair;
 
 public class Vecindario {
-    private int[] dimRows;
-    private int[] dimColumns;
+    private int radio=2;
+    private int centroX;
+    private int centroY;
+    private ArrayList<Pair<Integer,Integer>> puntos = new ArrayList<Pair<Integer,Integer>>();
     private ArrayList<Construcciones> construcciones;
     private ArrayList<Servicios> servicios;
     public static ArrayList<Vecindario> vecindarios;
+
+    public Vecindario(int centroX, int centroY) {
+        this.centroX = centroX;
+        this.centroY = centroY;
+        this.construcciones = new ArrayList();
+        this.servicios = new ArrayList();
+    }    
     
-    public static boolean crearVecindario(){
-        
-        return false;
+    public void makeCoodenadas(){
+        int r = radio;
+        while(r>0){
+            for(int i=0;i<2*r;i++){
+                Pair<Integer,Integer> pos = new Pair<>(centroX-r,(centroY-r)+i);
+                puntos.add(pos);
+                }
+            for(int i =0;i<2*r;i++){
+                Pair<Integer,Integer> pos = new Pair<>((centroX-r)+i,centroY+r);
+                puntos.add(pos);
+            }
+            for(int i=0;i<2*r;i++){
+                Pair<Integer,Integer> pos = new Pair<>(centroX+r,(centroY+r)-i);
+                puntos.add(pos);
+            }
+            for(int i=0;i<2*r;i++){
+                Pair<Integer,Integer> pos = new Pair<>((centroX+r)-i,centroY-r);
+                puntos.add(pos);
+            }
+            r--;
+        }
+        Pair<Integer,Integer> pos = new Pair<>(centroX,centroY);
+        puntos.add(pos);
+        }   
+
+    public ArrayList<Pair<Integer, Integer>> getPuntos() {
+        return puntos;
     }
     
+    public boolean compareCoordenadas(Vecindario v){
+        for(Pair<Integer,Integer> p2:v.getPuntos()){
+            for(Pair<Integer,Integer> p1:this.getPuntos()){
+                if(p1.equals(p2)){
+                    return false;
+                }
+            }
+        } 
+        return true;
+    }
 }
