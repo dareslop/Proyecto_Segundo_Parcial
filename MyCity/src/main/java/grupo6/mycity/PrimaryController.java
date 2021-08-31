aeb00ad8766f9a8b0ff3539b45c79ec579e6d943
dvega23 <87511013+dvega23@users.noreply.github.com>
2021-08-30 20:58:46 -0500

    Update PrimaryController.java

--- MyCity/src/main/java/grupo6/mycity/PrimaryController.java
+++ MyCity/src/main/java/grupo6/mycity/PrimaryController.java
@@ -1,8 +1,10 @@
 package grupo6.mycity;
 
 import Modelos.Ciudad;
+import Modelos.Usuario;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
@@ -11,6 +13,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
@@ -20,13 +23,14 @@ import javafx.scene.layout.StackPane;
  *
  * @author Emanuel Guerrero
  * @author Diego Vega
- * @author Darien L
+ * @author Dariel L.
  */
 public class GameController implements Initializable {
- 
+    public static Usuario u = SesionController.user;
+    public static ArrayList<Celda> nodos = new ArrayList<>();
+    public static Ciudad city = u.getCity();
     @FXML
-    private GridPane grdPane;
-    Ciudad city;
+    private GridPane grdPane;    
     @FXML
     private Label lbUser;
     @FXML
@@ -42,20 +46,24 @@ public class GameController implements Initializable {
     @FXML
     private Button btDemolicion;
     
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-             
-        
+        begin();
+        Thread t1 = new Thread(()->aumentarDias());
+        t1.start();        
     }
         
     public void begin(){
         for(int i = 0;i<30;i++){
             for(int j=0;j<18;j++){
-                ImageView img = new ImageView(new Image("Imagenes//grassTile.png",50,50,false,false));                
-                grdPane.add(img, i, j);
-            }       
-        Thread t1 = new Thread(()->aumentarDias());
-        t1.start();
+                ImageView img = new ImageView(new Image("Imagenes//grassTile.png",50,50,false,false));  
+                StackPane sckPane = new StackPane();
+                sckPane.getChildren().addAll(img);                
+                grdPane.add(sckPane, i, j);
+                nodos.add(new Celda(sckPane,j,i));
+            }               
         }
     }
     
@@ -63,7 +71,7 @@ public class GameController implements Initializable {
           while(true){
               try {
                   city.increaseDays();
-                  Platform.runLater(()->lbDias.setText("dias: "+city.getDias()));                  
+                  Platform.runLater(()->lbDias.setText("Dias: "+city.getDias()));                  
                   Thread.sleep(3000);
               } catch (InterruptedException ex) {
                   ex.printStackTrace();
@@ -72,8 +80,26 @@ public class GameController implements Initializable {
         }
     public void cobrarImpuestos(){            
         }
-    public void crearServicio(){
-        
+    public void crearServicio(){        
         }
-    }
+    
+    @FXML
+    private void mouseEntered(MouseEvent e) {
+        StackPane source = (StackPane)e.getSource() ;
+        Integer colIndex = GridPane.getColumnIndex(source);
+        Integer rowIndex = GridPane.getRowIndex(source);
+        System.out.printf("Mouse entered cell [%d, %d]%n", colIndex.intValue(), rowIndex.intValue()); 
+    } 
+}
+class Celda{
+    StackPane skPane;
+    int row;
+    int column;
+    public Celda(StackPane skPane, int row, int column) {
+        this.skPane = skPane;
+        this.row = row;
+        this.column = column;
+    }   
+}
+        
         
